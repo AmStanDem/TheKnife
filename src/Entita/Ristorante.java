3285641a7ed24b0754fac4098e77267484549be2
mzaro1 <mzaro1@studenti.uninsubria.it>
2025-05-21 12:27:40 +0200

    Aggiunta classe Ristorante.

--- src/Entita/Ristorante.java
+++ src/Entita/Ristorante.java
@@ -3,13 +3,13 @@ package Entita;
 public class Ristorante {
     private String nome;
     private String luogo;
-    private String tipoDiCucina;
+    private TipoCucina tipoDiCucina;
     private boolean delivery;
     private boolean prenotazione;
     private float prezzoMedio;
 
 
-    public Ristorante(String nome, String luogo, String tipoDiCucina, boolean delivery, boolean prenotazione, float prezzoMedio){
+    public Ristorante(String nome, String luogo, TipoCucina tipoDiCucina, boolean delivery, boolean prenotazione, float prezzoMedio){
         this.nome=nome;
         this.luogo=luogo;
         this.tipoDiCucina=tipoDiCucina;
@@ -20,7 +20,7 @@ public class Ristorante {
 
     public String getNOme(){return nome;}
     public String getLuogo(){return luogo;}
-    public String getTipoDiCucina(){return tipoDiCucina;}
+    public TipoCucina getTipoDiCucina(){return tipoDiCucina;}
     public boolean getDelivery(){return delivery;}
     public boolean getPrenotazione(){return prenotazione;}
     public float getPrezzoMedio(){return prezzoMedio;}
