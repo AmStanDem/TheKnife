1b3e602594bfed78cd97e44c40ade18e17fe7264
apesavento <apesavento@studenti.uninsubria.it>
2025-05-21 12:44:13 +0200

    Classe Recensione

--- src/Entita/Recensione.java
+++ src/Entita/Recensione.java
@@ -1,4 +1,36 @@
 package Entita;
 
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Recensione {
+
+    private Cliente cliente;
+    private int stelle;
+    private String recensione;
+    private LocalDateTime dataRecensione;
+
+    public Recensione(Cliente cliente, int stelle, String recensione) {
+        this.cliente = cliente;
+        this.stelle = stelle;
+        this.recensione = recensione;
+        this.dataRecensione = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        String formattedDateTime = dataRecensione.format(formatter);
+        return cliente.getUsername() + "(" + cliente.getCognome() + " " + cliente.getNome() + ")    Stelle: " +
+                stelle + "\nRecensione: " + recensione + "\nData di recensione: " + formattedDateTime;
+    }
+
+    // TESTING
+    public static void main(String[] args) {
+        Recensione recensione1 = new Recensione(new Cliente("Antonio","Pesavento","apesavento",
+                "ciao", LocalDate.of(2006,1,14),"Varese"),5,"ciao ciao");
+        System.out.println(recensione1);
+    }
+
 }
