0b3f3e1f45dbc417b7761eaee4a02f7cac041e13
thoma <thomasriotto95@gmail.com>
2025-06-05 17:04:46 +0200

    Aggiunti metodi I/O nella classe GestoreFile.java per il Ristorante.java. Modificata la classe Ristorante.java.

--- src/Entita/Ristorante.java
+++ src/Entita/Ristorante.java
@@ -2,29 +2,48 @@ package Entita;
 
 public class Ristorante {
     private String nome;
-    private String luogo;
+    private String nazione;
+    private String citta;
+    private String indirizzo;
+    private float latitudine;
+    private float longitudine;
     private TipoCucina tipoDiCucina;
     private boolean delivery;
     private boolean prenotazione;
     private float prezzoMedio;
+    private String descrizione;
 
 
-    public Ristorante(String nome, String luogo, TipoCucina tipoDiCucina, boolean delivery, boolean prenotazione, float prezzoMedio){
+    public Ristorante(String nome, String nazione, String citta, String indirizzo, float latitudine, float longitudine, TipoCucina tipoDiCucina, boolean delivery, boolean prenotazione, float prezzoMedio,  String descrizione) {
         this.nome=nome;
-        this.luogo=luogo;
+        this.nazione =nazione;
+        this.citta = citta;
+        this.indirizzo = indirizzo;
+        this.latitudine = latitudine;
+        this.longitudine = longitudine;
         this.tipoDiCucina=tipoDiCucina;
         this.delivery=delivery;
         this.prenotazione=prenotazione;
         this.prezzoMedio=prezzoMedio;
+        this.descrizione = descrizione;
     }
 
-    public String getNOme(){return nome;}
-    public String getLuogo(){return luogo;}
+    public Ristorante(String nome, String citta) {
+        this.nome=nome;
+        this.citta = citta;
+    }
+
+    public String getNome(){return nome;}
+    public String getNazione(){return nazione;}
+    public  String getCitta(){return citta;}
+    public String getIndirizzo(){return indirizzo;}
+    public float getLatitudine(){return latitudine;}
+    public float getLongitudine(){return longitudine;}
     public TipoCucina getTipoDiCucina(){return tipoDiCucina;}
     public boolean getDelivery(){return delivery;}
     public boolean getPrenotazione(){return prenotazione;}
     public float getPrezzoMedio(){return prezzoMedio;}
-
+    public  String getDescrizione(){return descrizione;}
 
 
 
