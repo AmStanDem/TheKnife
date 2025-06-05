433bbbee70021127f0dae745cb7adb7d38daa63e
thoma <thomasriotto95@gmail.com>
2025-06-05 16:15:34 +0200

    Aggiunta classe GestoreFile.java

---
+++ src/io_file/GestoreFile.java
@@ -0,0 +1,12 @@
+package io_file;
+
+import java.nio.file.Path;
+
+public class GestoreFile {
+    private final static Path fileUtenti = Path.of("data", "Utenti.csv");
+    private final static Path fileRistoranti = Path.of("data", "Ristoranti.csv");
+
+    private GestoreFile() {}
+
+    // METODI I/O
+}
