138e640b5001e5da9de67c98a4386b19de97507a
mzaro1 <mzaro1@studenti.uninsubria.it>
2025-06-23 16:54:56 +0200

    Aggiunta del metodo cercaRistorante in cui vengono visualizzate le informazioni principali e necessario per adre all'utente una risposta semplice ma completa

--- src/TheKnife.java
+++ src/TheKnife.java
@@ -1,5 +1,38 @@
+import io_file.GestoreFile;
+
 public class TheKnife {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+}
+
+/*cercaRistoranti*/
+public static LinkedList<Ritorante> cercaRistorante(boolean delivery, boolean prenotazione, float prezzoMinimo, float prezzoMassimo, float prezoMedio, String tipoDiCucina, double mediaStelle){
+    var ristoranti = GestoreFile.caricaRistoranti();
+    var risultati = new ArrayList<Ristorante>();
+    for(Ristorante ristorante : ristoranti){
+        if(ristorante.getPrezzioMedio() < prezzoMinimo){
+            continue;
+        }
+        if(!ristorante.getDelivery() == delivery){
+            continue;
+        }
+        if(!ristorante.getPrenotazione() == prenotazione){
+            continue;
+        }
+
+        if(!ristorante.getPRezzoMedio() ? < prezzoMinimo : > prezzoMassimo){
+            continue;
+        }
+        for(ristorante.getTipoDiCucina() : TipoCuina){
+            continue;
+        }
+
+        if(ristorante.getMediaStelle() < mediaStelle){
+          continue;
+        }
+
+
+        risultato.add(ristorante);
     }
 }
