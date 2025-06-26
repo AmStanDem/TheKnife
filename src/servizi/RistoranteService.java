e4ab2f2ab2812f8c9ab6839b1fedaab623c6a9b3
thoma <thomasriotto95@gmail.com>
2025-06-26 15:57:55 +0200

    Inizio implementazione del metodo cercaRistorante.

--- src/servizi/RistoranteService.java
+++ src/servizi/RistoranteService.java
@@ -1,12 +1,37 @@
 package servizi;
-import Entita.Ristorante;
-import Entita.Ristoratore;
+import Entita.*;
 import com.opencsv.exceptions.CsvException;
 import io_file.GestoreFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public final class RistoranteService {
+
+    public static ArrayList<Ristorante> cercaRistorante(TipoCucina tipoCucina, Localita localita, float prezzoMinimo, float prezzoMassimo, boolean delivery, boolean prenotazione, float mediaStelle) throws IOException, CsvException {
+        var ristoranti = GestoreFile.caricaRistoranti();
+
+        var risultato = new ArrayList<Ristorante>();
+
+        for (Ristorante ristorante : ristoranti) {
+            if (!filtroTipoCucina(ristorante, tipoCucina)) {
+                continue;
+            }
+
+
+            risultato.add(ristorante);
+        }
+
+        return risultato;
+    }
+
+    private static boolean filtroTipoCucina(Ristorante ristorante, TipoCucina tipoCucina) {
+        if (tipoCucina == null) {
+            return true;
+        }
+        return ristorante.getTipoDiCucina().equals(tipoCucina);
+    }
+
     public static boolean aggiungiRistorante(Ristoratore ristoratore, Ristorante ristorante) throws IOException, CsvException {
         if(ristoratore==null){
             return false;
@@ -20,7 +45,7 @@ public final class RistoranteService {
         return ristoratore.aggiungiRistorante(ristorante);
     }
 
-    public static void visualizzaRsitorante(Ristorante ristorante){
+    public static void visualizzaRistorante(Ristorante ristorante){
         System.out.println(ristorante);
     }
 
