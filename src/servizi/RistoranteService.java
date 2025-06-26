13490b33325185405e3c941fe67aec6eb1a6efc5
mzaro1 <mzaro1@studenti.uninsubria.it>
2025-06-26 15:33:27 +0200

    Aggiunta di RistoranteService

---
+++ src/servizi/RistoranteService.java
@@ -0,0 +1,22 @@
+package servizi;
+import Entita.Ristorante;
+import Entita.Ristoratore;
+import com.opencsv.exceptions.CsvException;
+import io_file.GestoreFile;
+
+import java.io.IOException;
+
+public final class RistoranteService {
+    public static boolean aggiungiRistorante(Ristoratore ristoratore, Ristorante ristorante) throws IOException, CsvException {
+        if(ristoratore==null){
+            return false;
+        }
+        if(ristorante==null){
+            return false;
+        }
+        if(!GestoreFile.aggiungiRistorante(ristorante)){
+            return false;
+        }
+        return ristoratore.aggiungiRistorante(ristorante);
+    }
+}
